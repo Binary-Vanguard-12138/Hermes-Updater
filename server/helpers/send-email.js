980efa113e78d5e7836a615e18ff9ca26b4cece0
David-Hang-12138 <david.hang.12138@gmail.com>
2023-01-09 02:58:52 -0500

    added outlook mailer

--- server/helpers/send-email.js
+++ server/helpers/send-email.js
@@ -1,6 +1,8 @@
 const config = require("config");
 const nodemailer = require("nodemailer");
+const nodeoutlook = require('nodejs-nodemailer-outlook');
 const { isProductionEnv } = require("./env");
+const logger = require("./logger");
 
 async function sendEmail({
     to,
@@ -13,10 +15,24 @@ async function sendEmail({
         // Don't send email in the development env.
         return;
     }
-    const transporter = nodemailer.createTransport(config.get("smtpOptions"));
-    // transporter.verify().then(console.log).catch(console.error);
     try {
+        /*
+        const transporter = nodemailer.createTransport(config.get("smtpOptions"));
+        // transporter.verify().then(console.log).catch(console.error);
         await transporter.sendMail({ from, to, subject, html, attachments });
+        */
+        await nodeoutlook.sendEmail({
+            auth: config.get("outlookSmtpOptions.auth"),
+            from,
+            to,
+            subject,
+            html,
+            text: html,
+            attachments,
+            // onError: (e) => logger.error(e),
+            onSuccess: (i) => logger.debug(i)
+        }
+        );
     } catch (err) {
         throw `Failed to send email, error=${err}`;
     }
