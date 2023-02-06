81209487708f22383b6b0d558b9c8f079212b1ca
David-Hang-12138 <david.hang.12138@gmail.com>
2023-02-07 07:55:27 +0800

    minor fix

--- server/service/user/product.js
+++ server/service/user/product.js
@@ -5,7 +5,7 @@ async function getProducts(from, size) {
     const lmt = getMongooseLimitParam(from, size);
     const cond = {};
     const total = await ProductModel.countDocuments(cond);
-    const products = await ProductModel.find(cond, "", lmt).sort({ created_at: -1 });
+    const products = await ProductModel.find(cond, "", lmt).sort({ updated_at: -1 });
     return { total, data: products };
 }
 
